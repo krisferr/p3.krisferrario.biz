8dea6816fb1ec1ae4e19fcf971fc0aa0bb410745
Kris Ferrario <kferrario@gmail.com>
2013-12-02 11:29:15 -0500

    code for end message

---
+++ js/features.js
@@ -0,0 +1,21 @@
+//$('.textures').click(function(){
+    
+    //var texture_that_was_clicked = $(this).css('background-image');
+    
+    //$('#canvas').css('background-image', texture_that_was_clicked);
+    
+//});
+
+
+$(document).ready(function(){
+  $("#hide").click(function(){
+    $("#overlay").hide();
+  });
+  $("#show").click(function(){
+    $("#overlay").show();
+  });
+});
+
+
+
+//object.style.backgroundImage="url(stars.gif)"
