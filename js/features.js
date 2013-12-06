ce2029edabe2980729172f87f5acd99c29ffd950
Kris Ferrario <kferrario@gmail.com>
2013-12-05 23:04:12 -0500

    Added functions to play audio

--- js/features.js
+++ js/features.js
@@ -11,6 +11,75 @@ $('.produce').click(function(){
     $('#clock-type').css('background-image', produce_clicked);
 });
 
+//User can select alarm that will play once when time is up.
+alarm = function(){
+    $('input[type=radio]').change(function(){
+        if($('#alarm_clock_bell_ringing').is(':checked'))
+        {
+            alarm = new Audio('../audio/alarm_clock_bell_ringing.mp3');
+    
+        }
+        else if($('#cuckoo_clock_striking_2_o_clock').is(':checked'))
+        {
+            alarm = new Audio('../audio/cuckoo_clock_striking_2_o_clock.mp3');
+    
+        }
+        else if ($('#grandfather_clock_striking_half_hour').is(':checked'))
+        {
+            alarm = new Audio('../audio/grandfather_clock_striking_half_hour.mp3');
+        
+        }
+        else if($('#12_clock_chime').is(':checked'))
+        {
+            alarm = new Audio('../audio/12_clock_chime.mp3');
+        }
+        else
+        {
+            alarm = null;
+        }
+        alarm.loop = false;
+        alarm.play();
+    });
+        
+};
+
+//User can select a tick-toc noise 
+$(document).ready(function(){
+    
+    $('input[type=radio]').change(function(){
+        
+        if($('#clock_tick_001').is(':checked'))
+        {
+            tickToc = new Audio('../audio/clock_tick_001.mp3');
+        }
+        
+        else if($('#clock_tick_002').is(':checked'))
+        {
+            tickToc = new Audio('../audio/clock_tick_002.mp3');
+        }
+        
+        else if ($('#electric_clock_ticking').is(':checked'))
+        {
+            tickToc = new Audio('../audio/electric_clock_ticking.mp3');
+        }
+        
+        else if($('#none').is(':checked'))
+        {
+            tickToc = null;
+        }
+        
+        else
+        {
+            //do nothing
+        }
+        
+        tickToc.loop = true;
+        tickToc.play();
+    });
+        
+});
+
+
 //Allows for end message (overlay) be be hidden when the user hits the "Ok" button
 $(document).ready(function(){ 
   $("#hide").click(function(){ 
@@ -20,3 +89,5 @@ $(document).ready(function(){
     $("#overlay").show(); 
   });
 });
+
+
